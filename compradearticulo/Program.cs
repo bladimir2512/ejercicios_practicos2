71936ba27bd62266d7fecfcb5733289e9f6be004
bladimir2512 <140455406+bladimir2512@users.noreply.github.com>
2023-10-09 15:55:34 -0600

    Add files via upload

---
+++ compradearticulo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace compradearticulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Compra de articulo");
+            Console.WriteLine("--------------------");
+
+            // Solicitar el precio del artículo
+            Console.Write("Ingrese el precio del artículo: ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+            // Solicitar la cantidad a comprar
+            Console.Write("Ingrese la cantidad que desea comprar: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            // Calcular el total a pagar
+            double total = precio * cantidad;
+
+            // Mostrar el resultado
+            Console.WriteLine("Total a pagar: " + total );
+
+            Console.WriteLine("¡Gracias por su compra!");
+
+            Console.ReadKey();
+        }
+    }
+}
+
